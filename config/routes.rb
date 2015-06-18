e31c739908daee9614ab32ce0586e813c0a9d097
kojikoji <kojima.t@pepabo.com>
2015-06-18 18:28:54 +0900

    コントローラの編集-Listing8.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,6 +8,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 
   # Example of regular route:
