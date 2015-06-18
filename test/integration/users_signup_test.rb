2d0914add948aeca9a56a7adf0a48b221deecc2e
kojikoji <kojima.t@pepabo.com>
2015-06-18 16:46:42 +0900

    flashのテストを追加

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
