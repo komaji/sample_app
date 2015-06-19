2e4e5c138f2e977801282de510e0b806689842f8
kojikoji <kojima.t@pepabo.com>
2015-06-19 15:30:39 +0900

    flash.nowによるログイン失敗のテストを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+  
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
   end
+  
   def destroy
   end
 end
