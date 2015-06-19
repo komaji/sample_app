3573080fedddabb3e6abb9d824f9e4155ad26bac
kojikoji <kojima.t@pepabo.com>
2015-06-19 17:49:22 +0900

    login完了

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -21,5 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                            password: "password",
                                            password_confirmation: "password" }
     end
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
