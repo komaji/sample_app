4be86b5c0efc4019332a5d6fd360f724b1cd61d5
kojikoji <kojima.t@pepabo.com>
2015-06-22 18:34:35 +0900

    checkbox完了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanetn[:remember_token] = user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   # Returns the user corresponding to the remember token cookie.
