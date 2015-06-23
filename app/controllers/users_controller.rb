ed0a127268c5d9c65430e955dd6e0f9102197e22
kojikoji <kojima.t@pepabo.com>
2015-06-23 17:07:47 +0900

    ページアクセスに対するセキュリティの向上

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :currect_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
 #    debugger
@@ -36,5 +38,22 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the currect user.
+  def currect_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
