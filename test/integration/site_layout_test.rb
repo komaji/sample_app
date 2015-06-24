f8e956072234dd9d385312fa8ef2a17b0dbc7d34
kojikoji <kojima.t@pepabo.com>
2015-06-24 16:23:35 +0900

    リンクのテストを追加

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,12 +1,34 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "layout links without login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links with login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
