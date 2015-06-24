25db0e6e9cb63ec51f726bed4b96ac847349e925
kojikoji <kojima.t@pepabo.com>
2015-06-24 14:22:38 +0900

    destroy actions を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :currect_user, only: [:edit, :update]
-
+  before_action :admin_user, only: :destroy
+  
   def index
     @users = User.paginate(page: params[:page])
   end    
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
   
   private
 
@@ -59,5 +66,10 @@ class UsersController < ApplicationController
   def currect_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
