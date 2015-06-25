d1b65154657ba6d58e06f1a61e37abd046d09ebf
kojikoji <kojima.t@pepabo.com>
2015-06-25 14:39:49 +0900

    site_layout_testのテスト名を変更

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,7 +6,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
   
-  test "layout links without login" do
+  test "layout links when not logged in" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -17,7 +17,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
   end
 
-  test "layout links with login" do
+  test "layout links when logged in" do
     log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
