a44042b6529dc7a39ee3d604f70ae7dac6e9098d
kojikoji <kojima.t@pepabo.com>
2015-06-26 12:05:02 +0900

    activatedのときだけログインできるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,11 +29,12 @@ class User < ActiveRecord::Base
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
-
+  
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
