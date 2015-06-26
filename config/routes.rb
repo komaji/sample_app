6734906474832876b288cae5e762999ec8341f3e
kojikoji <kojima.t@pepabo.com>
2015-06-26 16:52:44 +0900

    Add password resets & email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
@@ -9,4 +13,5 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
---
+++ db/migrate/20150626061510_add_sent_at_to_users.rb
@@ -0,0 +1,5 @@
+class AddSentAtToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :reset_sent_at, :datetime
+  end
+end
