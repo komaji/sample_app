5895bb24aa697529d357fadc9d3cd3b189d37f63
kojikoji <kojima.t@pepabo.com>
2015-07-02 16:52:48 +0900

    statsのテストに数の確認を追加

--- test/integration/static_pages_home_test.rb
+++ test/integration/static_pages_home_test.rb
@@ -11,12 +11,16 @@ class UsersHome < ActionDispatch::IntegrationTest
     get root_path
     assert_select "a[href=?]", following_user_path(@user)
     assert_select "a[href=?]", followers_user_path(@user)
+    assert_select 'strong#following', @user.following.count.to_s
+    assert_select 'strong#followers', @user.followers.count.to_s
   end
 
   test "stats when not logged in" do
     get root_path
     assert_select "a[href=?]", following_user_path(@user), count: 0
     assert_select "a[href=?]", followers_user_path(@user), count: 0
+    assert_select 'strong#following', count: 0 
+    assert_select 'strong#followers', count: 0
   end
   
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -16,8 +16,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_match @user.microposts.count.to_s, response.body
     assert_select "a[href=?]", following_user_path(@user)
     assert_select "a[href=?]", followers_user_path(@user)
-    assert_match @user.following.count.to_s, response.body
-    assert_match @user.followers.count.to_s, response.body
+    assert_select 'strong#following', @user.following.count.to_s
+    assert_select 'strong#followers', @user.followers.count.to_s
     assert_select 'div.pagination'
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
