2fe15e55881bbf2445e1b0d7fe69fd76b550deff
kojikoji <kojima.t@pepabo.com>
2015-07-02 15:46:19 +0900

    profileページのstatsのテストを修正

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -14,6 +14,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1', text: @user.name
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
+    assert_select "a[href=?]", following_user_path(@user)
+    assert_select "a[href=?]", followers_user_path(@user)
     assert_match @user.following.count.to_s, response.body
     assert_match @user.followers.count.to_s, response.body
     assert_select 'div.pagination'
